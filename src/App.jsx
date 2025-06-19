7b5379711b9d29806100796114e126e376a90bdd
小川　匠 <takuminn4646@gmail.com>
2025-06-19 20:02:36 +0900

    a

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import "leaflet/dist/leaflet.css";
 export default function App() {
     return (
         <div className="h-screen w-screen">
-            <MapContainer center={[35.0, 135.0]} zoom={10} className="h-full w-full">
+            <MapContainer center={[35.0, 135.0]} zoom={10} className="h-full w-full" style={{ height: "100vh", width: "100%" }} >
                 <TileLayer
                     attribution='&copy; OpenStreetMap contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
